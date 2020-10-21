ce1a096dd7f7bb445ddc25ed57fdec6917208591
jack riches <jack@intelligentinterfaces.co>
2020-10-21 02:29:12 +0100

    Add test and fix setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-jackric",
+    name="context-compose",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Jack Riches",
+    author_email="jackriches@gmail.com",
+    description="Compose context managers from a sequence",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jackric/sampleproject",
+    url="https://github.com/jackric/context_compose",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
-
--- tests/test_compose.py
+++ tests/test_compose.py
@@ -1,8 +1,12 @@
 from context_compose import compose, impotent_manager
 from contextlib import contextmanager
+from typing import List
 
 
 def test_impotent_manager():
+    """
+    The impotent manager is invoked without error
+    """
     x = 1
     with impotent_manager():
         x += 1
@@ -10,9 +14,42 @@ def test_impotent_manager():
 
 
 def test_compose_impotent_manager():
+    """
+    The impotent manager is composed twice without error
+    """
     x = 1
     mgr1 = impotent_manager()
     mgr2 = impotent_manager()
     with compose([mgr1, mgr2]):
         x += 1
     assert x == 2
+
+
+@contextmanager
+def logging_manager(name: str, log: List[str]):
+    log.append(f"Entered {name}")
+    yield
+    log.append(f"Exited {name}")
+
+
+def test_compose_enter_order():
+    """
+    The compose manager enters and exits context managers in
+    list order
+    """
+    log = []
+    mgr1 = logging_manager("A", log)
+    mgr2 = logging_manager("B", log)
+    mgr3 = logging_manager("C", log)
+
+    with compose([mgr1, mgr2, mgr3]):
+        pass
+
+    assert log == [
+        "Entered A",
+        "Entered B",
+        "Entered C",
+        "Exited C",
+        "Exited B",
+        "Exited A",
+    ]
